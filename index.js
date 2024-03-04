ef118a9360ea10826c1b493b653437c27bf2c821
Roberto Ramirez <robert.624@hotmail.com>
2024-03-03 23:20:01 -0500

    Add input validation and tests for capitalize and reverseString functions

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 // Group of functions to be tested
 
 function capitalize(string) {
+
+    if (typeof string !== 'string') {
+        throw new Error('Input must be a string')
+    }
+
   return string.charAt(0).toUpperCase() + string.slice(1)
 }
 
 function reverseString(string) {
+
+    if (typeof string !== 'string') {
+        throw new Error('Input must be a string')
+    }
     return string.split("").reverse().join("")
     }
 
--- index.test.js
+++ index.test.js
@@ -1,5 +1,37 @@
 const functions = require('./index')
 
-test('capitalize string', () => {
-    expect(functions.capitalize('hello')).toBe('Hello')
+describe('capitalize', () => {
+    test('capitalize first letter of string', () => {
+        expect(functions.capitalize('hello')).toBe('Hello')
+    })
+
+    test('capitalize first letter of string', () => {
+        expect(functions.capitalize('world')).toBe('World')
+    })
+
+    test('return empty string', () => {
+        expect(functions.capitalize('')).toBe('')
+    })
+
+    test('throw error if input is not a string', () => {
+        expect(() => functions.capitalize(1)).toThrow('Input must be a string')
+    })
+})
+
+describe('reverseString', () => {
+    test('reverse string', () => {
+        expect(functions.reverseString('hello')).toBe('olleh')
+    })
+
+    test('reverse string', () => {
+        expect(functions.reverseString('world')).toBe('dlrow')
+    })
+
+    test('return empty string', () => {
+        expect(functions.reverseString('')).toBe('')
+    })
+
+    test('throw error if input is not a string', () => {
+        expect(() => functions.reverseString(1)).toThrow('Input must be a string')
+    })
 })
